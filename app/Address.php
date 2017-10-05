8b2ee3730fd92d0d8829fc0f91540b14ee8b9d5c
Daniel Martins <daniel.anchieta@outlook.com>
2017-10-05 18:44:11 -0300

    'novo'

---
+++ app/Address.php
@@ -0,0 +1,19 @@
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model
+{
+    protected $fillable = [
+        'address',
+        'city',
+        'state',
+        'zipcode'
+    ];
+
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+}
